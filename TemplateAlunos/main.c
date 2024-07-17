149932f82d79fdc8e1712ebfaa372d706487aba3
Aurélio Gabriel Ortiz Vieira <163077031+aureliooortiz@users.noreply.github.com>
2024-07-17 20:23:30 -0300

    Update main.c

--- TemplateAlunos/main.c
+++ TemplateAlunos/main.c
@@ -23,50 +23,49 @@ int main() {
     char nome[MAX_CHAR];
     uint64_t numComp;
 	
-    int64_t tamVetor = 10;/*
+    int64_t tamVetor = 4;/*
     int* vetor = (int*)malloc(tamVetor * sizeof(int));
     if (vetor == NULL) {
         printf("Falha fatal. Impossível alocar memoria.");
         return 1;
-    }
-	*/
-	int vetor[10] = {38, 90, 90, 99, 63, 78, 98, 46, 28, 41};
+    }*/
+	
+	//int vetor[10] = {38, 90, 90, 99, 63, 78, 98, 46, 28, 41};
 	//int vetor[4] = {1, 50, -1, 10};
 	//int vetor[4] = {-1, 1, 10, 50};
-
+	int vetor[4] = {42, 15, 23, 8};
+	
     getNome(nome);
     printf("Trabalho de %s\n", nome);
     printf("GRR %u\n", getGRR());
 	
 	printf("tamVetor = %ld\n\n",tamVetor);
-	/*
+
 	// Recursivos
 	printf("Merge Sort Recursivo:\n");
 	aleatorizaVet(vetor, tamVetor);
-	//imprimeVet(vetor, tamVetor);
+	imprimeVet(vetor, tamVetor);
     numComp = mergeSort(vetor, tamVetor);
-    printf("NumComp: %ld\n", numComp);
-    //imprimeVet(vetor, tamVetor);
+    printf("\nNumComp: %ld\n", numComp);
+    imprimeVet(vetor, tamVetor);
     printf("\n");
-    
+    /*
     printf("Quick Sort Recursivo:\n");
     aleatorizaVet(vetor, tamVetor);
-	//imprimeVet(vetor, tamVetor);
+	imprimeVet(vetor, tamVetor);
     numComp = quickSort(vetor, tamVetor);
     printf("NumComp: %ld\n", numComp);
-    //imprimeVet(vetor, tamVetor);
+    imprimeVet(vetor, tamVetor);
     printf("\n");
-    */
     
     printf("Heap Sort Recursivo:\n");
-	//aleatorizaVet(vetor, tamVetor);
+	aleatorizaVet(vetor, tamVetor);
 	imprimeVet(vetor, tamVetor);    
     numComp = heapSort(vetor, tamVetor);
     printf("NumComp: %ld\n", numComp);
     imprimeVet(vetor, tamVetor);
     printf("\n");
     
-    /*
     // Iterativos
     printf("Merge Sort Iterativo:\n");
 	aleatorizaVet(vetor, tamVetor);
@@ -91,9 +90,8 @@ int main() {
     printf("NumComp: %ld\n", numComp);
     imprimeVet(vetor, tamVetor);
     printf("\n");
-	*/
 	
-    //free(vetor);
+    free(vetor);*/
 
     return 0;
 }
